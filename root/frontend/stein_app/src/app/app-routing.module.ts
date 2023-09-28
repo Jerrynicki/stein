b72b689ac16cb08003f7f2028d634636378f3af4
Leon Bojanowski <leongaborbojanowski04@gmail.com>
2023-09-28 23:51:00 +0200

    first login implementation (html & css only)

--- root/frontend/stein_app/src/app/app-routing.module.ts
+++ root/frontend/stein_app/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [ {path: 'login', component: LoginComponent}];
+const routes: Routes = [ {path: 'login', component: LoginComponent}, {path: 'registration', component: RegistrationComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- root/frontend/stein_app/src/app/app.module.ts
+++ root/frontend/stein_app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
